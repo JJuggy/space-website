85d7a994c67473af14a2cb737112f43647068cd2
Amiolemen David <Amidavid01@gmail.com>
2021-10-30 20:10:17 +0100

    added more styling

---
+++ index.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById('hamburger')
+const listContainer= document.querySelector('.list-container')
+
+   
+
+hamburger.addEventListener('click', ()=>{
+    console.log("i am clicked")
+    listContainer.classList.toggle("isOpen");
+})
+
