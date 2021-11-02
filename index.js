5dbb7b2869baece5bba5310fe30f57a531874228
Amiolemen David <Amidavid01@gmail.com>
2021-11-02 23:59:06 +0100

    fixed the issue of media query for the hamburger menu

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const hamburger = document.getElementById('hamburger')
 const listContainer= document.querySelector('.list-container')
+const navigationListContainer=document.querySelector(".navigation-links-container")
 
    
 
 hamburger.addEventListener('click', ()=>{
     console.log("i am clicked")
+    // navigationListContainer.style.display = "block"
     listContainer.classList.toggle("isOpen");
+    
 })
 
